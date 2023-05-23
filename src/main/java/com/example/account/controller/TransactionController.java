c291dbe9253701d4984188dd18b9d8b8b7bae7c3
postrel63 <postrel63@naver.com>
2023-05-23 10:37:19 +0900

    no message

--- src/main/java/com/example/account/controller/TransactionController.java
+++ src/main/java/com/example/account/controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.example.account.controller;
 
+import com.example.account.aop.AccountLock;
 import com.example.account.dto.CancelBalance;
 import com.example.account.dto.QueryTransactionResponse;
 import com.example.account.dto.TransactionDto;
@@ -26,6 +27,7 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping("/transaction/use")
+    @AccountLock
     public UseBalance.Response useBalance(
             @Valid @RequestBody UseBalance.Request request
     ) {
@@ -46,10 +48,12 @@ public class TransactionController {
     }
 
     @PostMapping("/transaction/cancel")
+    @AccountLock
     public CancelBalance.Response cancelBalance(
             @Valid @RequestBody CancelBalance.Request request
-    ) {
+    ) throws InterruptedException {
         try {
+            Thread.sleep(3000L);
             return CancelBalance.Response.from(
                     transactionService.cancelBalance(request.getTransactionId(),
                             request.getAccountNumber(), request.getAmount()));
